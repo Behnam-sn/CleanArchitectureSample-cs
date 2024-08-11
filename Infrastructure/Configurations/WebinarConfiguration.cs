121db9a668bce65414a7f234844e3cfa15d07615
Behnam Salehi <behnam.dev@outlook.com>
2024-08-11 15:29:21 +0330

    Create Infrastructure.csproj

---
+++ Infrastructure/Configurations/WebinarConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+internal sealed class WebinarConfiguration : IEntityTypeConfiguration<Webinar>
+{
+    public void Configure(EntityTypeBuilder<Webinar> builder)
+    {
+        builder.ToTable("Webinars");
+        builder.HasKey(i => i.Id);
+        builder.Property(i => i.Name).HasMaxLength(100).IsRequired();
+        builder.Property(i => i.ScheduledOn).IsRequired();
+    }
+}
---
+++ Infrastructure/Repositories/WebinarRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Abstractions;
+using Domain.Entities;
+
+namespace Infrastructure.Repositories;
+
+public sealed class WebinarRepository : IWebinarRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public WebinarRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public void Insert(Webinar webinar)
+    {
+        _dbContext.Set<Webinar>().Add(webinar);
+    }
+}
